690b1d7d229f24411435a67b3bade78926dd81b3
Bryce Counts <bryce.counts@icloud.com>
2026-02-09 13:39:35 -0600

    Delete all files

--- Backend/reset-db.js
+++
@@ -1,8 +0,0 @@
-/**
- * Reset the appointments database: deletes all appointments.
- * Run from the backend folder: node reset-db.js
- */
-const { db } = require('./db');
-
-const result = db.prepare('DELETE FROM appointments').run();
-console.log(`Deleted ${result.changes} appointment(s). Database is now empty.`);
